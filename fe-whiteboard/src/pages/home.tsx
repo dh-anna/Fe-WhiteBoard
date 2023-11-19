55438fc64fe72e91a9e708f1df15f7196ee3e432
KostyalBalint <kostyal.balintnak@gmail.com>
2023-11-19 14:56:13 +0100

    feat: homepage

--- fe-whiteboard/src/pages/home.tsx
+++ fe-whiteboard/src/pages/home.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import {NewWhiteboard} from "../components/newWhiteboard";
-import {ChooseWhiteboard} from "../components/chooseWhiteboard";
+import React from "react";
+import { NewWhiteboard } from "../components/newWhiteboard";
+import { ChooseWhiteboard } from "../components/chooseWhiteboard";
+import { Box, Paper, Stack, Typography } from "@mui/material";
 
 export const HomePage: React.FC = () => {
-    return (
-        <div>
-            <h1>Whiteboard App</h1>
-            <NewWhiteboard />
-            <ChooseWhiteboard />
-        </div>
-    );
+  return (
+    <Stack flexGrow={1} alignItems="center" p={2}>
+      <Paper elevation={2} sx={{ flexGrow: 1, width: "100%" }}>
+        <Stack direction="column" gap={3} m={2}>
+          <Typography variant="h3" textAlign="center">
+            Whiteboard App
+          </Typography>
+          <NewWhiteboard />
+          <ChooseWhiteboard />
+        </Stack>
+      </Paper>
+    </Stack>
+  );
 };
