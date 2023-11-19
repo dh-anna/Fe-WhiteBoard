ddb9ee87274e1b4931426c0dfcf6c093fb51d8da
dh-anna <dh01anna@gmail.com>
2023-11-19 11:53:42 +0100

    feat: crate default whiteboard

---
+++ fe-whiteboard/src/components/whiteboard.tsx
@@ -0,0 +1,62 @@
+// Whiteboard.tsx
+import React, { useRef, useEffect, useState } from 'react';
+import { Button, Paper } from '@mui/material';
+
+interface WhiteboardProps {
+    width: number;
+    height: number;
+}
+
+const Whiteboard: React.FC<WhiteboardProps> = ({ width, height }) => {
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
+
+    useEffect(() => {
+        if (canvasRef.current) {
+            const canvas = canvasRef.current;
+            canvas.width = width;
+            canvas.height = height;
+
+            const ctx = canvas.getContext('2d');
+            if (ctx) {
+                setContext(ctx);
+            }
+        }
+    }, [width, height]);
+
+    const handleClear = () => {
+        if (context) {
+            context.clearRect(0, 0, width, height);
+        }
+    };
+
+    const handleMouseDown = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+        if (context) {
+            context.beginPath();
+            context.moveTo(e.clientX - canvasRef.current!.offsetLeft, e.clientY - canvasRef.current!.offsetTop);
+        }
+    };
+
+    const handleMouseMove = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+        if (context) {
+            context.lineTo(e.clientX - canvasRef.current!.offsetLeft, e.clientY - canvasRef.current!.offsetTop);
+            context.stroke();
+        }
+    };
+
+    return (
+        <Paper elevation={3} style={{ margin: '20px', padding: '10px' }}>
+            <canvas
+                ref={canvasRef}
+                onMouseDown={handleMouseDown}
+                onMouseMove={handleMouseMove}
+                style={{ border: '1px solid #ccc' }}
+            />
+            <Button variant="contained" color="primary" onClick={handleClear} style={{ marginTop: '10px' }}>
+                Clear
+            </Button>
+        </Paper>
+    );
+};
+
+export default Whiteboard;
