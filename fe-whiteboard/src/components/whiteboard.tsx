b2936f0ba20598db9fad5a74d840e7684233d9be
dh-anna <dh01anna@gmail.com>
2023-11-19 12:09:01 +0100

    feat: eraser?

--- fe-whiteboard/src/components/whiteboard.tsx
+++ fe-whiteboard/src/components/whiteboard.tsx
@@ -1,16 +1,20 @@
 // Whiteboard.tsx
 import React, { useRef, useEffect, useState } from 'react';
 import { Button, Paper } from '@mui/material';
+import CreateIcon from '@mui/icons-material/Create';
 
 interface WhiteboardProps {
     width: number;
     height: number;
 }
 
+type DrawingTool = 'pen' | 'eraser';
+
 const Whiteboard: React.FC<WhiteboardProps> = ({ width, height }) => {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
     const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
     const [isDrawing, setIsDrawing] = useState(false);
+    const [currentTool, setCurrentTool] = useState<DrawingTool>('pen');
 
     useEffect(() => {
         if (canvasRef.current) {
@@ -41,13 +45,28 @@ const Whiteboard: React.FC<WhiteboardProps> = ({ width, height }) => {
 
     const handleMouseMove = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
         if (isDrawing && context) {
-            context.lineTo(e.clientX - canvasRef.current!.offsetLeft, e.clientY - canvasRef.current!.offsetTop);
-            context.stroke();
+            if (currentTool === 'pen') {
+                context.lineTo(e.clientX - canvasRef.current!.offsetLeft, e.clientY - canvasRef.current!.offsetTop);
+                context.stroke();
+            } else if (currentTool === 'eraser') {
+                // Use white color to simulate erasing
+                context.strokeStyle = '#fff';
+                context.lineWidth = 10;
+                context.lineTo(e.clientX - canvasRef.current!.offsetLeft, e.clientY - canvasRef.current!.offsetTop);
+                context.stroke();
+                // Reset the stroke style and line width
+                context.strokeStyle = '#000';
+                context.lineWidth = 2;
+            }
         }
     };
 
     const handleMouseUp = () => {
         setIsDrawing(false);
+    };
+
+    const switchTool = (tool: DrawingTool) => {
+        setCurrentTool(tool);
     };
 
     return (
@@ -59,9 +78,27 @@ const Whiteboard: React.FC<WhiteboardProps> = ({ width, height }) => {
                 onMouseUp={handleMouseUp}
                 style={{ border: '1px solid #ccc' }}
             />
-            <Button variant="contained" color="primary" onClick={handleClear} style={{ marginTop: '10px' }}>
-                Clear
-            </Button>
+            <div style={{ marginTop: '10px' }}>
+                <Button
+                    variant={currentTool === 'pen' ? 'contained' : 'outlined'}
+                    color="primary"
+                    onClick={() => switchTool('pen')}
+                >
+                    Pen
+                </Button>
+                <Button
+                    variant={currentTool === 'eraser' ? 'contained' : 'outlined'}
+                    color="secondary"
+                    onClick={() => switchTool('eraser')}
+                    style={{ marginLeft: '10px' }}
+                    startIcon={<CreateIcon />}
+                >
+                    Eraser
+                </Button>
+                <Button variant="contained" color="primary" onClick={handleClear} style={{ marginLeft: '10px' }}>
+                    Clear
+                </Button>
+            </div>
         </Paper>
     );
 };
