3a4b8ebb3cf362298016729a381fa959c6a98867
dh-anna <dh01anna@gmail.com>
2023-11-19 14:36:13 +0100

    feat: HomePage

--- fe-whiteboard/src/components/chooseWhiteboard.tsx
+++ fe-whiteboard/src/components/chooseWhiteboard.tsx
@@ -1,23 +1,141 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+    Box,
+    Button,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+} from "@mui/material";
 
 export const ChooseWhiteboard: React.FC = () => {
+    const [selectedWhiteboard, setSelectedWhiteboard] = useState<number | null>(
+        null
+    );
+    const [openDialog, setOpenDialog] = useState(false);
+
     const handleChooseWhiteboard = (id: number) => {
-        // Implement your logic for choosing a whiteboard
-        console.log(`Selected whiteboard ${id}`);
+        setSelectedWhiteboard(id);
+    };
+
+    const handleDeleteWhiteboard = () => {
+        // Implement your logic for deleting a whiteboard
+        console.log(`Deleted whiteboard ${selectedWhiteboard}`);
+        setSelectedWhiteboard(null);
+        handleCloseDialog();
+    };
+
+    const handleOpenDialog = () => {
+        setOpenDialog(true);
+    };
+
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
     };
 
     return (
-        <div>
-            <h2>Choose a Whiteboard</h2>
-            <ul>
-                <li>
-                    <button onClick={() => handleChooseWhiteboard(1)}>Whiteboard 1</button>
-                </li>
-                <li>
-                    <button onClick={() => handleChooseWhiteboard(2)}>Whiteboard 2</button>
-                </li>
+        <Box sx={styles.container}>
+            <h2 style={styles.title}>Choose a Whiteboard</h2>
+            <div style={styles.cardContainer}>
+                <Box sx={styles.card}>
+                    <Button
+                        style={styles.button}
+                        onClick={() => handleChooseWhiteboard(1)}
+                    >
+                        Whiteboard 1
+                    </Button>
+                    <Button
+                        sx={styles.deleteButton}
+                        onClick={() => {
+                            handleChooseWhiteboard(1);
+                            handleOpenDialog();
+                        }}
+                    >
+                        Delete
+                    </Button>
+                </Box>
+                <Box sx={styles.card}>
+                    <Button
+                        style={styles.button}
+                        onClick={() => handleChooseWhiteboard(2)}
+                    >
+                        Whiteboard 2
+                    </Button>
+                    <Button
+                        sx={styles.deleteButton}
+                        onClick={() => {
+                            handleChooseWhiteboard(2);
+                            handleOpenDialog();
+                        }}
+                    >
+                        Delete
+                    </Button>
+                </Box>
                 {/* Add more buttons for additional whiteboards */}
-            </ul>
-        </div>
+            </div>
+
+            {/* Confirmation Dialog */}
+            <Dialog open={openDialog} onClose={handleCloseDialog}>
+                <DialogTitle>Are you sure?</DialogTitle>
+                <DialogContent>
+                    <p>Do you really want to delete this whiteboard?</p>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseDialog} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleDeleteWhiteboard} color="primary">
+                        Yes, Delete
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </Box>
     );
+};
+
+const styles = {
+    container: {
+        textAlign: "center",
+        padding: "20px",
+        fontFamily: "Arial, sans-serif",
+    },
+    title: {
+        fontSize: "24px",
+        fontWeight: "bold",
+        marginBottom: "20px",
+    },
+    cardContainer: {
+        display: "flex",
+        justifyContent: "center",
+    },
+    card: {
+        border: "1px solid #ddd",
+        borderRadius: "8px",
+        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+        margin: "10px",
+        padding: "10px",
+        position: "relative",
+    },
+    button: {
+        padding: "10px 20px",
+        fontSize: "16px",
+        backgroundColor: "#3498db",
+        color: "#fff",
+        border: "none",
+        borderRadius: "5px",
+        cursor: "pointer",
+        marginRight: "8px",
+    },
+    deleteButton: {
+        position: "absolute",
+        top: "0px",
+        right: "0px",
+        padding: "5px 10px",
+        fontSize: "8px",
+        backgroundColor: "#e74c3c",
+        color: "#fff",
+        border: "none",
+        borderRadius: "5px",
+        cursor: "pointer",
+    },
 };
--- fe-whiteboard/src/components/newWhiteboard.tsx
+++ fe-whiteboard/src/components/newWhiteboard.tsx
@@ -1,16 +1,74 @@
 import React, { useState } from "react";
+import { Box, TextField, Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 
 export const NewWhiteboard: React.FC = () => {
   const [whiteboardId, setWhiteboardId] = useState<number | null>(null);
+  const [whiteboardName, setWhiteboardName] = useState<string>("");
 
   const handleNewWhiteboard = () => {
     const newId = Math.floor(Math.random() * 1000000);
     setWhiteboardId(newId);
   };
 
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setWhiteboardName(event.target.value);
+  };
+
   return (
-    <div>
-      <button onClick={handleNewWhiteboard}>Create New Whiteboard</button>
-    </div>
+      <Box sx={styles.container}>
+        <h2 style={styles.title}>Create New Whiteboard</h2>
+        <TextField
+            label="Whiteboard Name"
+            variant="outlined"
+            value={whiteboardName}
+            onChange={handleNameChange}
+            sx={styles.textInput}
+        />
+        <Button
+            style={styles.button}
+            onClick={handleNewWhiteboard}
+            variant="contained"
+            color="primary"
+            startIcon={<AddIcon />}
+        >
+          Create Whiteboard
+        </Button>
+        {whiteboardId && (
+            <p style={styles.message}>
+              New Whiteboard "{whiteboardName}" created with ID: {whiteboardId}
+            </p>
+        )}
+      </Box>
   );
+};
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: "20px",
+    fontFamily: "Arial, sans-serif",
+  },
+  title: {
+    fontSize: "24px",
+    fontWeight: "bold",
+    marginBottom: "20px",
+  },
+  textInput: {
+    width: "70%",
+    marginBottom: "20px",
+  },
+  button: {
+    padding: "10px",
+    fontSize: "16px",
+    borderRadius: "5px",
+    cursor: "pointer",
+  },
+  message: {
+    marginTop: "20px",
+    color: "#27ae60",
+  },
 };
